1bc4a50169677e418fab6bf0db60534a887e9c45
Pouya <pouya.ataei.7@gmail.com>
2025-01-27 21:47:45 +1300

    chore: bump version to 0.1.2

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.api.routes.router import router
 app = FastAPI(
     title="shallowresearchscholarspark",
     description="This is the service that handles the shallow research aspect of the scholar spark",
-    version="0.1.1"
+    version="0.1.2"
 )
 
 app.include_router(router)
